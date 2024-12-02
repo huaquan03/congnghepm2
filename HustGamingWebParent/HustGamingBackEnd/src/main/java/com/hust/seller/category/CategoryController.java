7d567674187a4114c7762142019e7f9cd201096f
Đỗ Kiên <kien.dt224869@sis.hust.edu.vn>
2024-12-02 16:11:11 +0700

    okee

--- HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/category/CategoryController.java
+++ HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/category/CategoryController.java
@@ -4,19 +4,21 @@ import org.springframework.ui.Model;
 import com.hust.seller.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/{name}")
+@RequestMapping("/categories/{name}")
 public class CategoryController {
     @Autowired
     CategoryService categoryService ;
+    @GetMapping("")
     public String getProductsByCategory(@PathVariable("name") String name, Model model) {
         List<Product> productList = categoryService.getProductByCategoryName(name) ;
-        if(productList ==null) return "not-found" ;
+        if(productList.size()==0) return "not-found" ;
         else {
             model.addAttribute("productList",productList) ;
             return "category" ;
--- HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/repository/CategoryRepository.java
+++ HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/repository/CategoryRepository.java
@@ -11,9 +11,10 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     List<Category> findAll();
-   Category findByCategoryId(int categoryId);
-   @Modifying
-   @Query("SELECT * FROM Product p, Category c WHERE p.CategoryID = c.CategoryID AND status =1 AND categoryName = :=name")
-   List<Product> findProductByCategoryName(@Param("name") String name) ;
+
+    Category findByCategoryId(int categoryId);
+
+    @Query("SELECT p FROM Product p JOIN Category c ON p.categoryID = c.categoryId WHERE  c.categoryName = :name")
+    List<Product> findProductByCategoryName(@Param("name") String name);
 
 }
