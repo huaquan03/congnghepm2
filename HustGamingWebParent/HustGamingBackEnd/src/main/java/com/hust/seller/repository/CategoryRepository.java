7f91a82612bb59e49db3307ad884d8756014f5a6
Nguyễn Văn Nam <131948589+Naman14741@users.noreply.github.com>
2024-12-06 15:42:31 +0700

    search finish 1

--- HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/repository/CategoryRepository.java
+++ HustGamingWebParent/HustGamingBackEnd/src/main/java/com/hust/seller/repository/CategoryRepository.java
@@ -1,12 +1,18 @@
 package com.hust.seller.repository;
 
 import com.hust.seller.entity.Category;
+import com.hust.seller.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     List<Category> findAll();
     Category findByCategoryId(int categoryId);
+
+    @Query("SELECT p FROM Product p JOIN Category c ON p.categoryID = c.categoryId WHERE  c.categoryName = :name")
+    List<Product> findProductByCategoryName(@Param("name") String name);
 
 }
